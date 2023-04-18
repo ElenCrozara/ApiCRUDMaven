84d16394764bb626226ee536b387c84b157baeff
elen <elen.crozzara@gmail.com>
2023-04-18 13:57:55 -0300

    Inclusão de um cadastro com POST

--- src/test/java/api/crud/maven/ApiTest.java
+++ src/test/java/api/crud/maven/ApiTest.java
@@ -1,2 +1,37 @@
-package api.crud.maven;public class ApiTest {
+package api.crud.maven;
+
+import org.junit.Test;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import static io.restassured.RestAssured.given;
+
+public class ApiTest {
+
+    // função de leitura do arquivo que contém o json
+    public String readJson(String pathJson) throws IOException {
+        return new String(Files.readAllBytes(Paths.get(pathJson)));
+    }
+
+    @Test // notação do Junit que declara uma stepe de teste
+    public void registerPerson() throws IOException {
+
+        String uri = "https://petstore.swagger.io/v2/pet";
+        String jsonBody = readJson("data/register1.json");
+
+        given()
+                .contentType("application/json")// tipo da informação
+                .log().all() // envie tudo da informação
+                .body(jsonBody) //jsonBody é a variável que guarda o caminho do arquivo que deve ser enviado
+
+        .when()
+                .post(uri)
+
+        .then()
+                .log().all()
+                .statusCode(200);
+
+
+    }
 }
