ac6c3de3af1ebea1c2669dc0947517474b2763e5
elen <elen.crozzara@gmail.com>
2023-04-22 13:12:45 -0300

    Completei o script consultPet (Get)

--- src/test/java/api/crud/maven/ApiTest.java
+++ src/test/java/api/crud/maven/ApiTest.java
@@ -5,44 +5,51 @@ import org.junit.Test;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.text.ParseException;
 
-import static com.sun.org.apache.xpath.internal.compiler.Token.contains;
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.CoreMatchers.*;
-import static org.hamcrest.Matchers.comparesEqualTo;
-import static org.hamcrest.Matchers.lessThan;
+import static org.hamcrest.Matchers.*;
 
 public class ApiTest {
     // função de leitura do arquivo que contém o json
     public String readJson(String pathJson) throws IOException {
         return new String(Files.readAllBytes(Paths.get(pathJson)));
     }
-    @Test // notação do Junit que declara uma stepe de teste
-    public void registerPerson() throws IOException { // metodo que não retorna nada mas executa algo
-        String uri = "https://petstore.swagger.io/v2/pet";
-        String jsonBody = readJson("data/register1.json");
+    String uri = "https://petstore.swagger.io/v2/pet";
 
+    @Test
+    public void registerPets() throws IOException, ParseException {
+        String jsonBody = readJson("data/register2.json");
         given()
-                .contentType("application/json")// tipo da informação
-                .log().all() // registrar tudo que eu to fazendo
-                .body(jsonBody) //jsonBody é a variável que guarda o caminho do arquivo que deve ser enviado
-
+                .contentType("application/json")
+                .log().all()
+                .body(jsonBody)
         .when()
                 .post(uri)
-
         .then()
                 .log().all()
                 .statusCode(200)
-                .body("id", equalTo(1984121100))
-                .body("name", equalTo("Preto"))
-                .body("status", equalTo("available"))
-                .time(lessThan(2000L))
-                .body("category.name", containsString("dog"))
-                .body("tags.id", hasItem(2023))
+                .body("name", is("Sara"))
+                .body("category.name", is("dog"))
+                .body("tags.id", hasItem(2022))
+                .body("status", is("available"));
+    }
+    @Test
+    public void consultPet() {
+        String petId = "16981";
 
-
-        ;
-
+        given()
+                .contentType("application/json")
+                .log().all()
+        .when()
+                .get(uri + "/" + petId)
+        .then()
+                .log().all()
+                .statusCode(200)
+                .time(lessThan(2000L));
+    }
+    @Test
+    public void deletePet() throws IOException {
 
 
     }
